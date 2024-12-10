fix vae and t5 for precision

diffsynth_engine/models/basic/attention.py
@@ -87,7 +87,7 @@ class Attention(nn.Module):
         if attn_mask is not None:
             attn_mask = repeat(attn_mask, "1 n ... -> (b n) ...", b=bs)
 
-        hidden_states = low_version_attention(q, k, v, attn_bias=attn_mask)
+        hidden_states = low_version_attention(q, k, v, attn_bias=attn_mask, scale=self.scale)
         hidden_states = rearrange(hidden_states, "(b n) s d -> b s (n d)", n=self.num_heads)
         hidden_states = hidden_states.to(q.dtype)
         hidden_states = self.to_out(hidden_states)

diffsynth_engine/models/components/vae.py
@@ -392,7 +392,7 @@ class VAEDecoder(PreTrainedModel):
             ResnetBlock(128, 128, eps=1e-6, device=device, dtype=dtype),
         ])
 
-        self.conv_norm_out = nn.GroupNorm(num_channels=128, num_groups=32, eps=1e-5, device=device, dtype=dtype)
+        self.conv_norm_out = nn.GroupNorm(num_channels=128, num_groups=32, eps=1e-6, device=device, dtype=dtype)
         self.conv_act = nn.SiLU()
         self.conv_out = nn.Conv2d(128, 3, kernel_size=3, padding=1, device=device, dtype=dtype)
 