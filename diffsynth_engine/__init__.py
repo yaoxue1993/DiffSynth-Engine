8fa7a91546a7ea1426bcb83cbc24478543709c6e
dizhipeng.dzp <dizhipeng.dzp@alibaba-inc.com>
2024-12-25 16:17:56 +0800

    add import

---
+++ diffsynth_engine/__init__.py
@@ -0,0 +1,2 @@
+from .utils.download import download_model
+from .pipelines import FluxImagePipeline, SDXLImagePipeline, SDImagePipeline
--- diffsynth_engine/pipelines/__init__.py
+++ diffsynth_engine/pipelines/__init__.py
@@ -1,3 +1,4 @@
 from .base import BasePipeline
 from .flux_image import FluxImagePipeline
 from .sdxl_image import SDXLImagePipeline
+from .sd_image import SDImagePipeline
