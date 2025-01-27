158357b3a3eb263c5994a9d90515814fbe117f29
yueyang.hyy <yueyang.hyy@alibaba-inc.com>
2025-01-27 16:08:15 +0800

    support RecifitedFlowBetaScheduler

--- diffsynth_engine/algorithm/noise_scheduler/flow_match/__init__.py
+++ diffsynth_engine/algorithm/noise_scheduler/flow_match/__init__.py
@@ -1,7 +1,9 @@
 from .recifited_flow import RecifitedFlowScheduler
 from .recifited_flow_ddim import RecifitedFlowDDIMScheduler
+from .recifited_flow_beta import RecifitedFlowBetaScheduler
 
 __all__ = [
     "RecifitedFlowScheduler",
-    "RecifitedFlowDDIMScheduler"
+    "RecifitedFlowDDIMScheduler",
+    "RecifitedFlowBetaScheduler"
 ]
