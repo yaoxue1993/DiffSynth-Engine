0de98e683405660c2bdee9d36f205e937bcd26e5
dizhipeng.dzp <dizhipeng.dzp@alibaba-inc.com>
2025-02-13 12:01:04 +0800

    bug fix

--- examples/flux_lora.py
+++ examples/flux_lora.py
@@ -1,15 +1,10 @@
 from diffsynth_engine import fetch_model, FluxImagePipeline, FluxModelConfig
 
-#model_path = fetch_model("muse/flux-with-vae", path="flux_with_vae.safetensors")
+model_path = fetch_model("muse/flux-with-vae", path="flux_with_vae.safetensors")
 lora_path = fetch_model("DonRat/MAJICFLUS_SuperChinesestyleheongsam", path="麦橘超国风旗袍.safetensors")
 
-basepath = "/home/admin/workspace/aop_lab/app_source/flux"
-config = FluxModelConfig(
-    model_path=f'{basepath}/majicFlus_v1.safetensors',
-    vae_path=f'{basepath}/ae.safetensors',
-    clip_path=f'{basepath}/clip_l_bf16.safetensors',
-    t5_path=f'{basepath}/t5xxl_v1_1_bf16.safetensors'    
-)
+
+config = FluxModelConfig(model_path=model_path)
 pipe = FluxImagePipeline.from_pretrained(config, device='cuda:0')
 pipe.patch_lora(path=lora_path, scale=1.0)
 image = pipe(prompt="a cat qipao")
--- examples/flux_text_to_image.py
+++ examples/flux_text_to_image.py
@@ -1,14 +1,8 @@
 from diffsynth_engine import fetch_model, FluxImagePipeline, FluxModelConfig
 
-#model_path = fetch_model("muse/flux-with-vae", path="flux_with_vae.safetensors")
+model_path = fetch_model("muse/flux-with-vae", path="flux_with_vae.safetensors")
 
-basepath = "/home/admin/workspace/aop_lab/app_source/flux"
-config = FluxModelConfig(
-    model_path=f'{basepath}/majicFlus_v1.safetensors',
-    vae_path=f'{basepath}/ae.safetensors',
-    clip_path=f'{basepath}/clip_l_bf16.safetensors',
-    t5_path=f'{basepath}/t5xxl_v1_1_bf16.safetensors'    
-)
+config = FluxModelConfig(model_path=model_path)
 pipe = FluxImagePipeline.from_pretrained(config, device='cuda:0')
 image = pipe(prompt="a cat")
 image.save("image.png")
