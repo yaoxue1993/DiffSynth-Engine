83751e6d6262fe946ed610d39b6bdba0e2cd242b
dizhipeng.dzp <dizhipeng.dzp@alibaba-inc.com>
2025-03-11 11:41:47 +0800

    bug fix

--- diffsynth_engine/utils/parallel.py
+++ diffsynth_engine/utils/parallel.py
@@ -4,11 +4,27 @@ import torch
 import torch.nn as nn
 import torch.multiprocessing as mp
 import torch.distributed as dist
-from torch.distributed.tensor.parallel import parallelize_module, ColwiseParallel, RowwiseParallel
+from torch.distributed.tensor.parallel import parallelize_module
 from torch.distributed.device_mesh import init_device_mesh
+
 
 logger = logging.getLogger(__name__)
 
+def wait_tensor(data):
+    if isinstance(data, tuple) or isinstance(data, list):
+        return [wait_tensor(t) for t in data]
+    elif isinstance(data, torch.Tensor):
+        return data.wait()
+    else:
+        return data
+
+def clone_tensor(data):
+    if isinstance(data, tuple) or isinstance(data, list):
+        return [clone_tensor(t) for t in data]
+    elif isinstance(data, torch.Tensor):
+        return data.clone()
+    else:
+        return data
 
 def _worker_loop(
     rank: int,
@@ -18,36 +34,41 @@ def _worker_loop(
     tp_plan: dict,
     device: str = "cuda",
 ):
+    """    
+    https://pytorch.org/docs/stable/multiprocessing.html#sharing-cuda-tensors
+    """
     try:
-        print("init process group")
         os.environ["RANK"] = str(rank)
         os.environ["WORLD_SIZE"] = str(world_size)
         os.environ["MASTER_ADDR"] = "localhost"
         os.environ["MASTER_PORT"] = "29500"
         torch.cuda.set_device(rank)
         dist.init_process_group(backend="nccl", init_method="env://", world_size=world_size, rank=rank)
-        print("init process group done")
-        print(f"rank: {rank}")
         tp_mesh = init_device_mesh(device, (world_size,))
         sharded_model = parallelize_module(model, tp_mesh, tp_plan)
-        print("parallelize module done")
         while True:
-            data = conn.recv()
-            print("recv data done")
-            if data == "EXIT":
+            if dist.get_rank() == 0:            
+                args, kwargs = conn.recv()
+                data = [args, kwargs]
+            else:
+                data = [None, None]
+            dist.broadcast_object_list(data, src=0)
+            if "TERMINATE" in data:
                 break
             args, kwargs = data
             result = sharded_model(*args, **kwargs)
-            print("forward done")
-            if rank == 0:
+            result = wait_tensor(result)
+            del args, kwargs            
+            if dist.get_rank() == 0:
                 conn.send(result)
             dist.barrier()
-
     except Exception as e:
         logger.error(f"Error in worker loop (rank {rank}): {e}")
     finally:
+        del sharded_model
+        conn.close()        
+        torch.cuda.synchronize()
         dist.destroy_process_group()
-        conn.close()
         
 
 class ParallelModel(torch.nn.Module):
@@ -61,19 +82,25 @@ class ParallelModel(torch.nn.Module):
         super().__init__()
         self.world_size = tp_size
         self.device = device
-        self.conn_main, self.conn_worker = mp.Pipe(duplex=True)
+        self.conn_main, conn_worker = mp.Pipe(duplex=True)
         mp.spawn(
             _worker_loop,
-            args=(self.world_size, self.conn_worker, model, tp_plan, device),
+            args=(self.world_size, conn_worker, model, tp_plan, device),
             nprocs=self.world_size,
             join=False,
         )
 
     def forward(self, *args, **kwargs):
-        print("start forward")
-        self.conn_main.send((args, kwargs))
-        return self.conn_main.recv()
-            
+        self.conn_main.send((args, kwargs))        
+        result = self.conn_main.recv()
+        return clone_tensor(result)
+    
+    def __del__(self):
+        # Send terminate signal to all workers
+        if hasattr(self, 'conn_main'):
+            self.conn_main.send(("TERMINATE", "TERMINATE"))
+            self.conn_main.close()
+                   
 
 __all__ = [
     "ParallelModel"
