6d8f243050f63e0e1fcf79632d4dcbba44c4b50f
Zhipeng DI <dizhipeng.dzp@alibaba-inc.com>
2025-04-30 16:16:49 +0800

    bug fix (#45)

--- diffsynth_engine/utils/parallel.py
+++ diffsynth_engine/utils/parallel.py
@@ -14,18 +14,25 @@ from datetime import timedelta
 from functools import partial
 from typing import Callable, Dict, List, Union, Optional
 from queue import Empty
-from yunchang.globals import Singleton, set_seq_parallel_pg
+
 
 from diffsynth_engine.utils import logging
 
 logger = logging.get_logger(__name__)
 
+class Singleton:
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super(Singleton, cls).__new__(cls, *args, **kwargs)
+        return cls._instance
 
 class ProcessGroupSingleton(Singleton):
     def __init__(self):
-        self.CFG_GROUP: dist.ProcessGroup = None
-        self.SP_GROUP: dist.ProcessGroup = None
-        self.TP_GROUP: dist.ProcessGroup = None
+        self.CFG_GROUP: Optional[dist.ProcessGroup] = None
+        self.SP_GROUP: Optional[dist.ProcessGroup] = None
+        self.TP_GROUP: Optional[dist.ProcessGroup] = None
 
         self.CFG_RANKS: List[int] = []
         self.SP_RANKS: List[int] = []
@@ -91,8 +98,9 @@ def init_parallel_pgs(
     rank: int = 0,
     world_size: int = 1,
 ):
+    from yunchang.globals import set_seq_parallel_pg
     sp_degree = sp_ulysses_degree * sp_ring_degree
-
+    
     assert sp_degree == 1 or tp_degree == 1, "not allowed to enable sequence parallel and tensor parallel together"
     assert world_size == cfg_degree * sp_degree * tp_degree, (
         f"world_size ({world_size}) must be equal to cfg_degree ({cfg_degree}) * sp_degree ({sp_degree}) * tp_degree ({tp_degree})"
